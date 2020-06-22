2eb99ee1e1cb134eb7cf9e9e4027952fe75ab95c
tudorpintea <tudorpintea98@gmail.com>
2020-06-22 18:51:34 +0300

    color screen done

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button, View, StyleSheet } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler';
 
 const ColorScreen = () => {
+
+    const [colors,setColors] = useState([]);
+    console.log(colors);
+
     return <View>
-        <Button title='Add a color'/>
+        <Button title='Add a color' onPress={() => {
+            setColors([...colors, randomRgb()]);
+        }} />
+
+
+        <FlatList 
+        keyExtractor={(item) => item}
+         data={colors}
+         
+         renderItem={({item}) => {
+             return <View style={{ height: 100, width: 100, backgroundColor: item }} />
+         }}
+        />
+
     </View>
+};
+
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})` ;
 };
 
 const styles=StyleSheet.create({});
